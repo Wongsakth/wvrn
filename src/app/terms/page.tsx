230876a2b13ec9d4cde94bbb2f7d15aca809fd0e
Wongsakth <mmrthai@gmail.com>
2026-05-13 21:17:15 +0700

    Add terms page.tsx

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,13 @@
+export default function TermsPage() {
+  return (
+    <div className="max-w-3xl mx-auto py-20 px-6">
+      <h1 className="text-3xl font-bold mb-4">
+        Terms of Service
+      </h1>
+
+      <p className="text-zinc-400">
+        WVRN Terms of Service
+      </p>
+    </div>
+  )
+}
